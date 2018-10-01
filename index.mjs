897a5f661ba60f3c4e79f28a6729b0a76c85ef1a
Minifyre <minifyre@gmail.com>
2018-10-01 18:28:21 -0500

    +rgba dom

--- index.mjs
+++ index.mjs
@@ -1,13 +1,12 @@
 import silo from './output.mjs'
 import truth from './node_modules/truth/truth.mjs'
-const {config,input,logic,output,util}=silo
+const {config,input,logic,output,util,v}=silo
 
 export default async function color(url='/node_modules/color-picker/')
 {
 	const
 	[css]=await util.importFiles([url+'index.css'])
 	config.css=css
-	console.log(silo)
 
 	customElements.define('color-picker',color.picker)
 }
@@ -25,6 +24,5 @@ color.picker=class extends HTMLElement
 		this.dom=output(this)
 		v.flatUpdate(shadow,this.dom)
 		renderer=()=>output.render(this)
-		//@todo copy resize observer from code-editor & integrate it here
 	}
 }
--- output.mjs
+++ output.mjs
@@ -2,9 +2,52 @@ import silo from './input.mjs'
 import v from './node_modules/v/v.mjs'
 silo.output=function(state)
 {
-	//generate v-based dom here
+	return [v('style',{},silo.config.css),
+		v('fieldset',{},
+			v('legend',{},'rgba'),
+			output.slider('Red',{min:0,max:255,value:153}),
+			output.slider('Green',{min:0,max:255,value:153}),
+			output.slider('Blue',{min:0,max:255,value:153}),
+			output.slider('Alpha',{min:0,max:100,value:100}),
+		)
+	]
 }
+
 silo.v=v
+
+/*
+named colors
+#rgb
+#rgba
+#rrggbb
+#rrggbbaa
+rgb()
+rgba()
+hsl()
+hsla()
+
+[css4?](https://developer.mozilla.org/en-US/docs/Web/CSS/color#Specifications)
+color()
+cmyk()
+hwb()
+*/
+
 const {config,input,logic,output,util}=silo
+
+output.slider=function(label,opts)
+{
+	const props=Object.assign({min:0,max:100,value:50},opts)
+	return v('.row',{},
+		v('label',{},label),
+		v('button',{},'-'),
+		v('input',{...props,type:'range'}),
+		v('button',{},'+'),
+		v('input',{...props,type:'number'})
+	)
+}
+output.render=function()
+{
+	
+}
 
 export default silo
