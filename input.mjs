4e5f99fc13b5d644772cd2dbfc34644fee28b273
Minifyre <minifyre@gmail.com>
2018-10-02 11:37:39 -0500

    +input.increment
    Input increment will increment the values of a neighboring range element (and the input[number] label as well) according to a pre-determined increment.

--- input.mjs
+++ input.mjs
@@ -6,4 +6,17 @@ silo.input=function(evt,picker)
 }
 const {config,input,logic,util}=silo
 
+input.increment=function(evt)
+{
+	const
+	{path,target}=evt,
+	type=target.getAttribute('data-name'),
+	[inc]=util.elAttrs2Nums(target,'value'),
+	range=target.parentElement.querySelector('[type="range"]'),
+	[max,min,oldVal]=util.elAttrs2Nums(range,'max,min,value'),
+	newVal=Math.max(min,Math.min(max,oldVal+inc)),
+	editor=path.find(x=>(x.tagName||'').toLowerCase()==='color-picker')
+	if(''+newVal==='NaN') console.log({min,max,oldVal,inc,newVal})
+	if(oldVal!==newVal) editor.state[type]=newVal
+}
 export default silo
