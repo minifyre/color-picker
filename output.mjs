6f0d18400fdf4ab46ca394b6ec1835595652f65b
Minifyre <minifyre@gmail.com>
2018-10-02 10:44:45 -0500

    added canvas-based gradients

--- output.mjs
+++ output.mjs
@@ -6,26 +6,43 @@ output=function output(editor)
 {
 	const
 	{r,b,g,a}=editor.state,
-	color=util.color.rgba(r,g,b,a),
+	color=util.color.rgba(r,g,b,a/100),
 	update=evt=>input(evt,editor),
 	props=(value,max=255)=>({min:0,max,value,on:{input:update}})
 	return [v('style',{},silo.config.css),
 		v('fieldset',{},
 			v('legend',{style:`background-color:${color};`},color),
-			output.slider('Red',props(r),util.gradientR(r,g,b,a)),
-			output.slider('Green',props(g),util.gradientG(r,g,b,a)),
-			output.slider('Blue',props(b),util.gradientB(r,g,b,a)),
-			output.slider('Alpha',props(a,100),util.gradientA(r,g,b,a)),
+			output.slider('Red',props(r)),
+			output.slider('Green',props(g)),
+			output.slider('Blue',props(b)),
+			output.slider('Alpha',props(a,100)),
 		)
 	]
 }
 silo.output=output
 silo.v=v
 output.render=function(picker)//@todo make this more performant!
-{
+{//@todo switch to canvas backgrounds!
 	const newDom=output(picker)
 	v.flatUpdate(picker.shadowRoot,newDom,picker.dom)
 	this.dom=newDom
+
+	console.clear()
+	const
+	{r,g,b,a}=picker.state,
+	tmp=[...picker.shadowRoot.querySelectorAll('canvas')]
+	.forEach(function(canvas)
+	{
+		const
+		ctx=canvas.getContext('2d'),
+		color=canvas.getAttribute('data-name'),
+		fn=`gradient${color.toUpperCase()}`,
+		gradient=ctx.createLinearGradient(0,0,255,1)
+		util[fn](r,g,b,a).forEach((color,i)=>gradient.addColorStop(i,color))
+		ctx.fillStyle=gradient
+		ctx.clearRect(0,0,255,1)
+		ctx.fillRect(0,0,255,1)
+	})
 }
 /*
 named colors
@@ -45,7 +62,7 @@ hwb()
 */
 
 
-output.slider=function(label,opts,style)
+output.slider=function(label,opts)
 {
 	const
 	props=Object.assign({min:0,max:100,value:50},opts),
@@ -53,7 +70,8 @@ output.slider=function(label,opts,style)
 	return v('.row',{},
 		v('label',{},label),
 		v('button',{},'-'),
-		v('span.gradient',{style},
+		v('span.gradient',{},
+			v('canvas',{data:{name},height:1,width:255}),
 			v('input',{...props,data:{name},type:'range'})
 		),
 		v('button',{},'+'),
