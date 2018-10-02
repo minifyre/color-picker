560d905a41ce215f116f951a80f919d633c95c9b
Minifyre <minifyre@gmail.com>
2018-10-02 11:56:13 -0500

    spunoff state update
    Moved state update into a sub function so that it can be called multiple times (until user releases the button).

--- input.mjs
+++ input.mjs
@@ -8,15 +8,20 @@ const {config,input,logic,util}=silo
 
 
 input.increment=function(evt)
-{
+{//@todo fix bug where 0 displays as one in [type=number]
 	const
 	{path,target}=evt,
 	type=target.getAttribute('data-name'),
 	[inc]=util.elAttrs2Nums(target,'value'),
 	range=target.parentElement.querySelector('[type="range"]'),
-	[max,min,oldVal]=util.elAttrs2Nums(range,'max,min,value'),
-	newVal=util.numWithinRange(min,oldVal+inc,max),
-	editor=path.find(x=>(x.tagName||'').toLowerCase()==='color-picker')
-	if(oldVal!==newVal) editor.state[type]=newVal
+	editor=path.find(x=>(x.tagName||'').toLowerCase()==='color-picker'),
+	updateUntilRelease=function()
+	{
+		const
+		[max,min,oldVal]=util.elAttrs2Nums(range,'max,min,value'),
+		newVal=util.numWithinRange(min,oldVal+inc,max)
+		if(oldVal!==newVal) editor.state[type]=newVal
+	}
+	updateUntilRelease()
 }
 export default silo
