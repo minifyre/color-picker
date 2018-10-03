04130fb8e66b948db4f4ac1b1e022cc830c6c6da
Minifyre <minifyre@gmail.com>
2018-10-02 19:55:19 -0500

    simplified functions

--- color.mjs
+++ color.mjs
@@ -4,148 +4,73 @@ export default function logic()
 }
 logic.hslaStr=(h,s,l,a=1)=>`hsla(${h},${s}%,${l}%,${a})`,
 logic.rgbaStr=(r,g,b,a=1)=>`rgba(${r},${g},${b},${a})`,
-
-
-
-
 logic.hex2rgb=function(hex)
 {
 	//fcc to ffcccc
 	if(hex.length===3) hex=hex.split('').map(char=>char+''+char).join('')
 	//[r,g,b]
 	return hex.match(/#?(.{2})(.{2})(.{2})/).slice(1).map(str=>parseInt(str,16))
-},
-
-
-color=
-{
-	
-	hexToHsl:function(hex)
-	{
-		return this.rgbToHsl(this.logic.hex2rgb(hex));
-	},
-	rgbToHex:function(rgb)
-	{
-		var c,hex,i;
-		if(typeof rgb==='string')
-		{
-			rgb=this.isRgb(rgb);
-		}
-		if(rgb)
-		{
-			hex=(function()
-			{
-				var j,len,ref,results;
-				ref=rgb.slice(0,3);
-				results=[];
-				for (j=0,len=ref.length; j<len; j++)
-				{
-					c=ref[j];
-					results.push(parseFloat(c).toString(16));
-				}
-				return results;
-			})();
-			hex=(function()
-			{
-				var j,len,results;
-				results=[];
-				for (j=0,len=hex.length; j<len; j++)
-				{
-					c=hex[j];
-					if(c.length===1)
-					{
-						results.push(`0${c}`);
-					}
-					else
-					{
-						results.push(c);
-					}
-				}
-				return results;
-			})();
-			hex=hex.join('');
-			if(_.compact((function()
-				{
-					var j,len,ref,results;
-					ref=hex.match(/.{1,2}/g);
-					results=[];
-					for (j=0,len=ref.length; j<len; j++)
-					{
-						i=ref[j];
-						results.push(i[0]===i[1]);
-					}
-					return results;
-				})()).length===3)
-			{
-				return `#${hex[0]}${hex[2]}${hex[4]}`;
-			}
-			else
-			{
-				return `#${hex}`;
-			}
-		}
-	},
-	rgbToHsl:function(rgb)
-	{
-		var a,add,b,diff,g,h,hue,l,lum,max,min,r,s,sat;
-		if(typeof rgb==='string')
-		{
-			rgb=this.isRgb(rgb);
-		}
-		if(!rgb)
-		{
-			return false;
-		}
-		r=parseFloat(rgb[0])/255;
-		g=parseFloat(rgb[1])/255;
-		b=parseFloat(rgb[2])/255;
-		max=Math.max(r,g,b);
-		min=Math.min(r,g,b);
-		diff=max-min;
-		add=max+min;
-		hue=min===max ? 0 :r===max ? ((60*(g-b)/diff)+360) % 360 :g===max ? (60*(b-r)/diff)+120 :(60*(r-g)/diff)+240;
-		lum=0.5*add;
-		sat=lum===0 ? 0 :lum===1 ? 1 :lum <=0.5 ? diff/add :diff/(2-add);
-		h=Math.round(hue);
-		s=Math.round(sat*100);
-		l=Math.round(lum*100);
-		a=parseFloat(rgb[3])||1;
-		return [h,s,l,a];
-	},
-	hslToRgb:function(hsl)
-	{
-		var a,b,bt,g,gt,hue,lum,p,q,r,rt,sat;
-		if(typeof hsl==='string') hsl=this.isHsl(hsl)
-		if(!hsl) return false
-		hue=parseInt(hsl[0])/360
-		sat=parseInt(hsl[1])/100
-		lum=parseInt(hsl[2])/100
-		q=lum <=.5 ? lum*(1+sat) :lum+sat-(lum*sat)
-		p=2*lum-q
-		rt=hue+(1/3)
-		gt=hue
-		bt=hue-(1/3)
-		r=Math.round(this.hue2rgb(p,q,rt)*255)
-		g=Math.round(this.hue2rgb(p,q,gt)*255)
-		b=Math.round(this.hue2rgb(p,q,bt)*255)	
-		a=parseFloat(hsl[3])||1
-		return [r,g,b,a]
-	},
-	hslToHex:function(hsl)
-	{
-		if(typeof hsl==='string') hsl=this.isHsl(hsl)
-		if(!hsl) return false
-		return this.rgbToHex(this.hslToRgb(hsl))
-	},
-	hue2rgb:function(p,q,h)
-	{
-		h+=	h<0?1:
-			h>1?-1:
-				0
-		return	h*6<1?p+(q-p)*h*6:
-				h*2<1?q:
-				h*3<2?p+(q-p)*((2/3)-h)*6:
-				p
-	}
 }
-export default color
+logic.hexToHsl=hex=>logic.rgb2hsl(logic.hex2rgb(hex))
+logic.rgba2hexa=(...args)=>'#'+args.map(num=>num.toString(16)).join('')
+logic.rgba2hsla=function(r,g,b,a=1)
+{
+	r/=255
+	g/=255
+	b/=255
+
+	const
+	max=Math.max(r,g,b),
+	min=Math.min(r,g,b),
+	diff=max-min,
+	sum=max+min,
+
+	hue=min===max?0:
+		r===max?((60*(g-b)/diff)+360)%360:
+		g===max?(60*(b-r)/diff)+120:
+				(60*(r-g)/diff)+240,
+
+	lum=0.5*sum,
+
+	sat=lum===0?0:
+		lum===1?1:
+		lum<=0.5?diff/sum:
+		diff/(2-sum)
+
+	return [...[hue,sat*100,lum*100].map(Math.round),a]
+}
+logic.hsl2rgb=function(h,s,l,a=1)
+{
+	const
+	[hue,sat,lum]=[h/360,s/100,l/100],
+
+	q=	lum<=.5?lum*(1+sat):
+				lum+sat-(lum*sat),
+	p=2*lum-q,
+	rt=hue+(1/3),
+	gt=hue,
+	bt=hue-(1/3),
+
+	[r,g,b]=
+	[
+		[rp,q,rt],
+		[p,q,gt],
+		[p,q,bt]
+	]
+	.map(logic.hue2rgb)
+	.map(num=>num*255)
+	.map(Math.round)
+
+	return [r,g,b,a]
+}
+logic.hslToHex=(h,s,l)=>logic.rgb2hex(logic.hsl2rgb(h,s,l))
+logic.hue2rgb=function(p,q,h)
+{
+	h+=	h<0?1:
+		h>1?-1:
+			0
+	return	h*6<1?p+(q-p)*h*6:
+			h*2<1?q:
+			h*3<2?p+(q-p)*((2/3)-h)*6:
+			p
+}
