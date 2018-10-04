c7654e831319fbfd571a06ef7b02f81aaa35a441
Minifyre <minifyre@gmail.com>
2018-10-04 11:20:22 -0500

    +evt2customEl

--- util.mjs
+++ util.mjs
@@ -19,5 +19,6 @@ util.gradientG=(r,g,b,a)=>[rgba(r,0,b,a),rgba(r,255,b,a)]
 util.gradientB=(r,g,b,a)=>[rgba(r,g,0,a),rgba(r,g,255,a)]
 util.gradientA=(r,g,b,a)=>[rgba(r,g,b,0),rgba(r,g,b,100)]
 
+util.evt2customEl=({path})=>path.find(x=>(x.tagName||'').match('-'))
 
 export default util
