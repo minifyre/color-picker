4a5ae9afbe81ecc524197845049e1a8324de9d8b
Minifyre <minifyre@gmail.com>
2018-12-09 20:37:03 -0600

    bug fix
    +destructuring statements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import silo from './node_modules/silo/index.js'
 import truth from './node_modules/truth/truth.mjs'
 import v from './node_modules/v/v.mjs'
 
+const {config,util,logic,output,input}=silo
+
 export default silo(async function color(url='/node_modules/color-picker/')
 {
 	const
